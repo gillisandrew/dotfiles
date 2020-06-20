efea49959fc8644a1cf1a526a05f204b6687a048
Andrew Gillis <gillis.andrew@gmail.com>
2020-06-20 12:49:55 -0400

    add gitconfig

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 OHMYZSH_CUSTOM_GITHUB_REPO="gillisandrew/ohmyzsh-custom"
+sudo apt update
+sudo apt install socat zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 rm -rf "$ZSH/custom/*"
 git clone "https://github.com/$OHMYZSH_CUSTOM_GITHUB_REPO.git" "$ZSH/custom"
