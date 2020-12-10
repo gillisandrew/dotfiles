31b370e8ff4783da6901299f2b4e6b2a77e178bd
Andrew Gillis <gillis.andrew@gmail.com>
2020-12-10 13:40:53 -0500

    restructure

--- install.sh
+++ install.sh
@@ -3,23 +3,32 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OHMYZSH_CUSTOM_GITHUB_REPO="gillisandrew/ohmyzsh-custom"
 NAME_LOWER=$(echo $NAME | tr '[:upper:]' '[:lower:]')
 EMAIL="gillis.andrew+$NAME_LOWER@gmail.com"
-# sudo apt update
-# sudo apt install socat zsh
-# ssh-keygen -f "$HOME/.ssh/${NAME_LOWER}_rsa" -t rsa -b 4096 -C "$EMAIL"
 
 
+sudo apt update
+sudo apt install socat zsh
 
-# if [[ ! -d "$HOME/.oh-my-zsh" ]] then
-#     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#     rm -rf "$ZSH/custom/*"
-#     git clone "https://github.com/$OHMYZSH_CUSTOM_GITHUB_REPO.git" "$ZSH/custom" --recurse-submodules --remote-submodules
-# fi
 
-for filename in $DIR/.dotfiles/.*; do
-    if test -f "$filename"; then
+if [[ ! -f "$HOME/.ssh/${NAME_LOWER}_rsa" ]]; then
+    ssh-keygen -f "$HOME/.ssh/${NAME_LOWER}_rsa" -t rsa -b 4096 -C "$EMAIL"
+fi
+
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    rm -rf "$ZSH/custom/*"
+    git clone "https://github.com/$OHMYZSH_CUSTOM_GITHUB_REPO.git" "$ZSH/custom" --recurse-submodules --remote-submodules
+fi
+
+for filename in $DIR/linux/.*; do
+    if [[ -f "$filename" ]]; then
         basename="$(basename -- $filename)"
-        echo $basename
-        mv "$HOME/$basename" "$HOME/$basename.bkp"
-        ln -s $filename "$HOME/$basename"
+
+        if [[ -f "$HOME/$basename" && ! -L "$HOME/$basename" ]]; then
+            mv "$HOME/$basename" "$HOME/$basename.bkp"
+        else 
+            rm "$HOME/$basename"
+        fi
+
+        ln -s $filename "$HOME/$basename"        
     fi
 done
--- linux/.profile
+++ linux/.profile
@@ -16,6 +16,14 @@ if [ -n "$BASH_VERSION" ]; then
     fi
 fi
 
+# if running zsh
+if [ -n "$ZSH_VERSION" ]; then
+    # include .zshrc if it exists
+    if [ -f "$HOME/.zshrc" ]; then
+	. "$HOME/.zshrc"
+    fi
+fi
+
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
@@ -24,4 +32,9 @@ fi
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/.local/bin" ] ; then
     PATH="$HOME/.local/bin:$PATH"
+fi
+
+# set PATH so it includes composer vendor bin if it exists
+if [ -d "$HOME/.composer/vendor/bin" ] ; then
+    PATH="$HOME/.composer/vendor/bin:$PATH"
 fi
